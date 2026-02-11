c3b4127fbda1254ca1cca5cd3c1da0c5633078aa
bbwcQWE <776627967@qq.com>
2026-02-11 12:25:47 +0800

    修复版权信息，更新为Team11319 Polaris及其GitHub链接

--- src/main/java/frc/robot/subsystems/bline/BLinePathFollower.java
+++ src/main/java/frc/robot/subsystems/bline/BLinePathFollower.java
@@ -1,5 +1,5 @@
-// Copyright (c) 2021-2026 Littleton Robotics
-// http://github.com/Mechanical-Advantage
+// Copyright (c) 2025-2026 Team11319 Polaris
+// https://github.com/bbwcQWE
 //
 // Use of this source code is governed by a BSD
 // license that can be found in the LICENSE file
