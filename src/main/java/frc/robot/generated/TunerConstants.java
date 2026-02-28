395b7befeca42534472ad6d26d601f8ca3a65a99
bbwcQWE <776627967@qq.com>
2026-02-28 21:08:33 +0800

    调整TunerConstants中的steerGains和driveGains的PID增益，以提高控制精度

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -22,9 +22,9 @@ public class TunerConstants {
   // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
   private static final Slot0Configs steerGains =
       new Slot0Configs()
-          .withKP(25)
+          .withKP(85)
           .withKI(0)
-          .withKD(0.1)
+          .withKD(0.5)
           .withKS(0.1)
           .withKV(1.16)
           .withKA(0)
@@ -32,7 +32,7 @@ public class TunerConstants {
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains =
-      new Slot0Configs().withKP(.001).withKI(0).withKD(0).withKS(0.2234).withKV(0.79659);
+      new Slot0Configs().withKP(.01).withKI(0).withKD(0).withKS(0.2234).withKV(0.79659);
 
   // The closed-loop output type to use for the steer motors;
   // This affects the PID/FF gains for the steer motors
